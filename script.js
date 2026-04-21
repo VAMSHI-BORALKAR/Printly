e73305ff5a617d37654463f28fab7f3e2a8a1ed8
Vamshi Boralkar <vamshirameshboralkar@gmail.com>
2026-04-22 00:08:34 +0530

    Enhance homepage layout and styling for improved user experience added buttons for view products and expore colors

--- script.js
+++ script.js
@@ -39,3 +39,19 @@ faqQuestions.forEach(question => {
     }
   });
 });
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    const targetId = this.getAttribute('href');
+    const targetSection = document.querySelector(targetId);
+
+    if (targetSection) {
+      window.scrollTo({
+        top: targetSection.offsetTop - 40,
+        behavior: 'smooth'
+      });
+    }
+  });
+});
